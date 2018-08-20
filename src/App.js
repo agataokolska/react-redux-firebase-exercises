e6b9cc3ac93963b67a29ba3eed606ca96a138a4c
agata okolska <agata.okolska@gmail.com>
2018-08-20 16:23:25 +0200

    add users component, add link and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import SmartComponent from './SmartComponent'
+import Users from './Users'
 
 class App extends Component {
   render() {
@@ -12,9 +13,12 @@ class App extends Component {
             <Link to={'/hello'}>Hello</Link>
             <Link to={'/hello/world'}>World</Link>
             <Link to={'/smart'}>Smart</Link>
+            <Link to={'/users'}>Users</Link>
+
             <Route path={'/hello/world'} component={World} />
             <Route path={'/smart'} component={SmartComponent} />
             <Route path={'/hello'} component={Hello} />
+            <Route path={'/users'} component={Users} />
             <Route path={'/'} exact component={Hello} />
           </div>
         </Router>
